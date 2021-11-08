d615242cd42c03126f8880bc8da99aa086b14e76
Dorota Czaplejewicz <dorota.czaplejewicz@puri.sm>
2021-11-08 11:51:33 +0000

    readme

--- src/postproc.h
+++ src/postproc.h
@@ -1,1 +1,5 @@
+/// This calls out to Rust because the glib version fails to reap processes sometimes,
+/// causing the spinner to spin forever.
+/// I decided it's easiest to solve the race in Rust
+/// after considering debugging glib or rolling out a raw threading solution.
 void spawn_post_process_task(char processing_script[256], char burst_dir[23], char capture_fname[255], void *thumb);
